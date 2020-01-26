e906c3f45d1c3a1fcf19411ea64604e8a2737c0e
ismcodifiquer <60313089+ismcodifiquer@users.noreply.github.com>
2020-01-26 18:10:49 +0100

    Objetos

---
+++ objetos.py
@@ -0,0 +1,12 @@
+import turtle
+
+tortuguita = turtle.Turtle()
+otraTortuguita = turtle.Turtle()
+
+tortuguita.shape('turtle')
+tortuguita.fd(50)
+
+otraTortuguita.shape('turtle')
+otraTortuguita.color('green')
+otraTortuguita.left(90)
+otraTortuguita.fd(50)
