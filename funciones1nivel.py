68757c325393a1c3ad71e1d6d215ac8a276e80bb
ismcodifiquer <60313089+ismcodifiquer@users.noreply.github.com>
2020-01-26 16:30:29 +0100

    Lambdas

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -4,11 +4,17 @@ def normal(i):
 def cuadrado(x):
     return x * x
 
+def cubo(x):
+    return x**3
+
 def sumaTodos(limitTo, f):
     resultado = 0
     for i in range(limitTo+1):
         resultado += f(i)
     return resultado
 
-print(sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+if __name__ == '__main__':
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3, cuadrado))
+else:
+    print(__name__)
